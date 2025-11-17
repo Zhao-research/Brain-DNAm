a03a2bab8503e75875d7809fb4c6c0ff82c1069b
Zhao-research <165067721+Zhao-research@users.noreply.github.com>
2025-11-17 18:57:15 -0500

    Update DMR.sh

--- DMR.sh
+++ DMR.sh
@@ -1,3 +1,18 @@
+#!/bin/sh
+#SBATCH --job-name=DMR #Job name
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=gmiao@usf.edu # Where to send mail
+#SBATCH --error=batch_%j.err
+#SBATCH --output=batch_%j.out
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5Gb
+#SBATCH --account=group
+#SBATCH --qos=group
+#SBATCH --time=100:00:00       
+#SBATCH --array=1
+date;hostname;pwd
+
 module load combined-pvalues
 #https://github.com/brentp/combined-pvalues/tree/master/examples
 
